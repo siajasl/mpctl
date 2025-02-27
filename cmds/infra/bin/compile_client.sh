f231d1abf2f5d3d1c507803dce4856bb70768e9f
Mark A. Greenslade <siajasl-0@pm.me>
2025-02-27 13:53:28 +0100

    1. Compilation support.

--- cmds/infra/bin/compile_client.sh
+++ cmds/infra/bin/compile_client.sh
@@ -22,25 +22,9 @@ function _help() {
 
 function _main()
 {
-    local mode=${1}
-    local path_to_monorepo="$(get_path_to_monorepo)"
-    local path_to_build="$path_to_monorepo/iris-mpc"
+    local build_mode=${1}
 
-    if [ ! -d "$path_to_monorepo" ]; then
-        log "ERROR: monorepo must be cloned into $(get_path_to_parent) before compilation can occur"
-    else
-        if [ ! -d "$path_to_build" ]; then
-            log "ERROR: invalid build path: $path_to_build"
-        else
-            pushd $path_to_build
-            if [ "$mode" = "debug" ]; then
-                cargo build --bin client
-            else
-                cargo build --bin client --release
-            fi
-            popd || exit
-        fi
-    fi
+    do_build_binary "iris-mpc1" "client" "$build_mode"
 }
 
 # ----------------------------------------------------------------
---
+++ utils/infra.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#######################################
+# Builds a binary.
+#######################################
+function do_build_binary()
+{
+    local build_subdir=${1}
+    local build_target=${2}
+    local build_mode=${3}
+
+    local build_path="$(get_path_to_monorepo)/$build_subdir"
+    if [ ! -d "$build_path" ]; then
+        log "ERROR: invalid build path: $build_path"
+        return
+    fi
+
+    pushd $build_path
+    if [ "$build_mode" == "debug" ]; then
+        cargo build --bin $build_target
+    else
+        cargo build --bin $build_target $build_mode
+    fi
+    popd || exit
+}
