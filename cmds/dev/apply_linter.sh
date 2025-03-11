98ad9c85daa802bf3da5d0f5f0f398ea56404de4
Mark A. Greenslade <siajasl-0@pm.me>
2025-03-11 14:16:35 +0100

    1. New commands to bring up/down net.

---
+++ cmds/dev/apply_linter.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+function _help() {
+    echo "
+    COMMAND
+    ----------------------------------------------------------------
+    mpctl-code-apply-linter
+
+    DESCRIPTION
+    ----------------------------------------------------------------
+    Applies linting prior to a commit.
+    "
+}
+
+function _main()
+{
+    pushd "$(get_path_to_monorepo)" || exit
+
+    cargo fmt --all
+    cargo clippy --all-targets --all-features -- -D warnings --no-deps
+
+    popd | exit
+}
+
+# ----------------------------------------------------------------
+# ENTRY POINT
+# ----------------------------------------------------------------
+
+source "$MPCTL"/utils/main.sh
+
+unset _HELP
+
+for ARGUMENT in "$@"
+do
+    KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
+    VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)
+    case "$KEY" in
+        help) _HELP="show" ;;
+        *)
+    esac
+done
+
+if [ "${_HELP:-""}" = "show" ]; then
+    _help
+else
+    _main
+fi
