2d29de5e0170c0e150a5af2579bb0fde8e768f4a
Mark A. Greenslade <siajasl-0@pm.me>
2025-05-03 14:14:04 +0200

    1. Added new commands to run jobs that generate and utilise synthetic iris data.

---
+++ cmds/jobs/init_plain_text_iris_file.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+function _help() {
+    echo "
+    COMMAND
+    ----------------------------------------------------------------
+    mpctl-job-init-plain-text-iris-file
+
+    DESCRIPTION
+    ----------------------------------------------------------------
+    Initializes plain text iris files.
+    "
+}
+
+function _main()
+{
+    pushd "$(get_path_to_monorepo)" || exit
+
+    # Run job.
+    cargo run --bin generate_benchmark_data
+
+    # Copy output.
+    rm -rf "$MPCTL/data/iris-plaintext"
+    cp -r "$(pwd)/iris-mpc-cpu/data" "$MPCTL/data/iris-plaintext"
+
+    popd || exit
+}
+
+# ----------------------------------------------------------------
+# ENTRY POINT
+# ----------------------------------------------------------------
+
+source "$MPCTL"/utils/main.sh
+
+unset _HELP
+
+for ARGUMENT in "$@"
+do
+    KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
+    VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)
+    case "$KEY" in
+        help) _HELP="show" ;;
+        *)
+    esac
+done
+
+if [ "${_HELP:-""}" = "show" ]; then
+    _help
+else
+    _main
+fi
